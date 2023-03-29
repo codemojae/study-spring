5d0f97f59e41dcfa0e2ab62621fadc7d336066b6
codemojae <116280283+codemojae@users.noreply.github.com>
2023-03-29 10:15:13 +0900

    backup

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+// 모델인거네.
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
